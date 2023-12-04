64ee7e4312f6cf62303c4b3c35d7dc96019ef888
geebytes <vforfreedom96@gmail.com>
2023-12-04 11:27:09 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,10 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .paddleocr import *
-
+import fitz
 __version__ = paddleocr.VERSION
 __all__ = [
     'PaddleOCR', 'PPStructure', 'draw_ocr', 'draw_structure_result',
     'save_structure_res', 'download_with_progressbar', 'sorted_layout_boxes',
     'convert_info_docx', 'to_excel'
 ]
+fitz.restore_aliases()
